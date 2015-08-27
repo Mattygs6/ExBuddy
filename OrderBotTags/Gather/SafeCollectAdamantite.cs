019d9ec40baacd11bfdc00b09cef0dea58334865
Matt Gramolini <mattygs@gmail.com>
2015-08-26 22:25:21 -0400

    Collect adamantite

---
+++ OrderBotTags/Gather/SafeCollectAdamantite.cs
@@ -0,0 +1,242 @@
+﻿namespace ExBuddy.OrderBotTags.Gather
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Buddy.Coroutines;
+
+    using Clio.Utilities;
+    using Clio.XmlEngine;
+
+    using ff14bot;
+    using ff14bot.Behavior;
+    using ff14bot.Enums;
+    using ff14bot.Helpers;
+    using ff14bot.Managers;
+    using ff14bot.Navigation;
+    using ff14bot.NeoProfiles;
+    using ff14bot.Objects;
+
+    using TreeSharp;
+
+    [XmlElement("SafeCollectAdamantite")]
+    public class SafeCollectAdamantite : ProfileBehavior
+    {
+        enum Abilities : ushort
+        {
+            Stealth = 229,
+            SharpVision = 235,
+            TruthOfMountains = 238,
+            CollectorsGlove = 4074,
+            MethodicalAppraisal = 4075,
+            ImpulsiveAppraisal = 4077,
+            DiscerningEye = 4078,
+            SingleMind = 4084
+        }
+
+        private Vector3 initialLocation;
+        private bool isDone;
+
+        public override bool IsDone
+        {
+            get
+            {
+                return isDone;
+            }
+        }
+
+        [DefaultValue(-1)]
+        [XmlAttribute("TeleportId")]
+        public int TeleportId { get; set; }
+
+        protected override void OnStart()
+        {
+            this.initialLocation = Core.Player.Location;
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(ctx => Gather());
+        }
+
+        private async Task<bool> Gather()
+        {
+            var node =
+                GameObjectManager.GetObjectsOfType<GatheringPointObject>()
+                    .FirstOrDefault(
+                        gpo =>
+                        gpo.EnglishName == "Unspoiled Mineral Deposit"
+                        && gpo.IsVisible && gpo.IsValid);
+
+            if (node == null)
+            {
+                return false;
+            }
+
+            MovementManager.SetFacing2D(node.Location);
+            var safeSpot = GetSafeSpot(Core.Player.Heading);
+
+            var fp = new FlightPathTo { Target = safeSpot.Item1, Radius = 3.0f, NavHeight = 10.0f, MountId = 45, Smoothing = 0.2f, DismountAtDestination = true, LogWaypoints = true };
+            fp.Start();
+            await fp.Fly();
+            await Coroutine.Wait(Timeout.Infinite, () => fp.IsDone);
+
+            //Stealth
+            await Coroutine.Wait(3000, () => Actionmanager.CanCast((uint)Abilities.Stealth, Core.Player));
+            Actionmanager.DoAction((uint)Abilities.Stealth, Core.Player);
+
+            //Truth of Mountains
+            if (!Core.Player.HasAura(222))
+            {
+                await Coroutine.Wait(3000, () => Actionmanager.CanCast((uint)Abilities.TruthOfMountains, Core.Player));
+                Actionmanager.DoAction((uint)Abilities.TruthOfMountains, Core.Player);
+            }
+
+            //Collector's Glove
+            if (!Core.Player.HasAura(805))
+            {
+                await Coroutine.Wait(3000, () => Actionmanager.CanCast((uint)Abilities.CollectorsGlove, Core.Player));
+                Actionmanager.DoAction((uint)Abilities.CollectorsGlove, Core.Player);
+            }
+
+            //Move to node
+            await Coroutine.Wait(
+                20000,
+                () =>
+                    {
+                        var result = Navigator.NavigationProvider.MoveTo(safeSpot.Item2);
+
+                        return result == MoveResult.Done || result == MoveResult.ReachedDestination;
+                    });
+
+            Poi.Current = new Poi(node, PoiType.Gather);
+            Poi.Current.Unit.Interact();
+
+            await Coroutine.Wait(5000, () => GatheringManager.WindowOpen);
+
+            await Coroutine.Sleep(1000);
+            GatheringManager.GetGatheringItemByIndex(5).GatherItem();
+
+            await Coroutine.Sleep(4000);
+            GatheringManager.GetGatheringItemByIndex(5).GatherItem();
+
+            await Coroutine.Sleep(500);
+            RaptureAtkUnitManager.Update();
+            await Coroutine.Wait(5000, () => RaptureAtkUnitManager.GetWindowByName("GatheringMasterpiece") != null);
+
+            await Cast(Abilities.DiscerningEye);
+            await Cast(Abilities.ImpulsiveAppraisal);
+
+            if (Core.Player.HasAura(757))
+            {
+                await Cast(Abilities.SingleMind);
+            }
+            else
+            {
+                await Cast(Abilities.DiscerningEye);
+            }
+
+            await Cast(Abilities.ImpulsiveAppraisal);
+
+            if (Core.Player.HasAura(757))
+            {
+                await Cast(Abilities.SingleMind);
+            }
+            else
+            {
+                await Cast(Abilities.DiscerningEye);
+            }
+
+            await Cast(Abilities.MethodicalAppraisal);
+
+            if (Core.Player.CurrentGP >= 50)
+            {
+                await Cast(Abilities.SharpVision);
+            }
+
+            while (GatheringManager.SwingsRemaining > 0)
+            {
+                await Coroutine.Sleep(500);
+
+                RaptureAtkUnitManager.Update();
+
+                await Coroutine.Wait(5000, () => RaptureAtkUnitManager.GetWindowByName("GatheringMasterpiece") != null);
+                while (!ff14bot.RemoteWindows.SelectYesNoItem.IsOpen)
+                {
+                    RaptureAtkUnitManager.GetWindowByName("GatheringMasterpiece").SendAction(1, 1, 0);
+                    await Coroutine.Wait(1000, () => ff14bot.RemoteWindows.SelectYesNoItem.IsOpen);
+                }
+
+                ff14bot.RemoteWindows.SelectYesNoItem.Yes();
+                await Coroutine.Wait(5000, () => !ff14bot.RemoteWindows.SelectYesNoItem.IsOpen);
+
+                await Coroutine.Sleep(500);
+            }
+
+            Poi.Clear("Gather Complete!");
+
+            if (TeleportId == -1)
+            {
+                await Coroutine.Wait(
+                    20000,
+                    () =>
+                    {
+                        var result = Navigator.NavigationProvider.MoveTo(safeSpot.Item1);
+
+                        return result == MoveResult.Done || result == MoveResult.ReachedDestination;
+                    });
+
+                fp = new FlightPathTo { Target = initialLocation, Radius = 3.0f, NavHeight = 25.0f, MountId = 45, Smoothing = 0.2f, DismountAtDestination = false, LogWaypoints = true };
+                fp.Start();
+                await fp.Fly();
+                await Coroutine.Wait(Timeout.Infinite, () => fp.IsDone);
+
+                isDone = true;
+                return true;
+            }
+
+            WorldManager.TeleportById((uint)TeleportId);
+            isDone = true;
+            return true;
+        }
+
+        private Tuple<Vector3, Vector3> GetSafeSpot(float heading)
+        {
+            if (heading >= 5.7 && heading < 1.5)
+            {
+                return new Tuple<Vector3, Vector3>(new Vector3(84.4492f, -1.340709f, -790.6899f), new Vector3(79.85004f, -0.2340581f, -805.6276f));
+            }
+            if (heading >= 1.5 && heading < 3.6)
+            {
+                return new Tuple<Vector3, Vector3>(new Vector3(64.96072f, -2.004024f, -870.546571f), new Vector3(71.80842f, -0.29418f, -856.2421f));
+            }
+
+            return new Tuple<Vector3, Vector3>(new Vector3(20.19515f, -2.000566f, -821.9562f), new Vector3(24.25113f, -0.01400313f, -832.7057f));
+        }
+
+        private async Task<bool> Cast(uint id)
+        {
+            //Wait till we can cast the spell
+            await Coroutine.Wait(5000, () => Actionmanager.CanCast(id, Core.Player));
+            var result = Actionmanager.DoAction(id, Core.Player);
+            //Wait till we can cast methodical again
+            await Coroutine.Wait(5000, () => Actionmanager.CanCast(4075, Core.Player));
+            //Wait for aura?
+            await Coroutine.Sleep(300);
+            return result;
+        }
+
+        private async Task<bool> Cast(Abilities id)
+        {
+            return await Cast((uint)id);
+        }
+    }
+}
---
+++ OrderBotTags/Imported/FlightPathTo.cs
@@ -0,0 +1,207 @@
+namespace ff14bot.NeoProfiles
+{
+    using ff14bot.Interfaces;
+    using Buddy.Coroutines;
+    using Clio.Utilities;
+    using Clio.XmlEngine;
+    using ff14bot.Managers;
+    using ff14bot.Helpers;
+    using ff14bot.Navigation;
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using TreeSharp;
+
+    using System.Collections.Generic;
+
+    [XmlElement("FlightPathTo")]
+    public class FlightPathTo : ProfileBehavior
+    {
+        private bool isDone = false;
+        private readonly IPlayerMover playerMover = new SlideMover();
+        private List<Vector3> waypoints = new List<Vector3>();
+
+        public override bool IsDone { get { return isDone; } }
+
+        [XmlAttribute("XYZ")]
+        public Vector3 Target { get; set; }
+
+        [XmlAttribute("Radius")]
+        public float Radius { get; set; }
+
+        [XmlAttribute("Smoothing")]
+        public float Smoothing { get; set; }
+
+        [DefaultValue(45)]
+        [XmlAttribute("MountId")]
+        public int MountId { get; set; }
+
+        [DefaultValue(20.0f)]
+        [XmlAttribute("NavHeight")]
+        public float NavHeight { get; set; }
+
+        [DefaultValue(true)]
+        [XmlAttribute("DismountAtDestination")]
+        public bool DismountAtDestination { get; set; }
+
+        [XmlAttribute("LogWaypoints")]
+        public bool LogWaypoints { get; set; }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => Fly());
+        }
+
+        public async Task<bool> Fly()
+        {
+            await FindWaypoints(Target);
+
+            if (waypoints.Count > 0)
+            {
+                foreach (var waypoint in waypoints)
+                {
+                    if (LogWaypoints)
+                    {
+                        Logging.Write("Moving to waypoint: {0}", waypoint);
+                    }
+                    await MoveToWithinRadius(waypoint, Radius);
+                }
+            }
+            else
+            {
+                Logging.Write("No viable path computed for {0}.", Target);
+            }
+
+            if (DismountAtDestination)
+            {
+                await DescendAndDismount();
+            }
+
+            isDone = true;
+            return true;
+        }
+
+        public static float Lerp(float value1, float value2, float amount)
+        {
+            return value1 + (value2 - value1) * amount;
+        }
+
+        public static Vector3 Lerp(Vector3 value1, Vector3 value2, float amount)
+        {
+            return new Vector3(
+                Lerp(value1.X, value2.X, amount),
+                Lerp(value1.Y, value2.Y, amount),
+                Lerp(value1.Z, value2.Z, amount));
+        }
+
+        public static float Distance2D(Vector3 l, Vector3 r)
+        {
+            return Convert.ToSingle(Math.Sqrt((r.X - l.X) * (r.X - l.X) + (r.Z - l.Z) * (r.Z - l.Z)));
+        }
+
+        public static async Task<Vector3> SampleParabola(Vector3 start, Vector3 end, float height, float t)
+        {
+            if (Math.Abs(start.Y - end.Y) < 0.1f)
+            {
+                //start and end are roughly level, pretend they are - simpler solution with less steps
+                Vector3 travelDirection = end - start;
+                Vector3 computed = start + t * travelDirection;
+                computed.Y += (float)(Math.Sin((double)(t * (float)Math.PI))) * height;
+                return computed;
+            }
+            else
+            {
+                //start and end are not level, gets more complicated
+                Vector3 travelDirection = end - start;
+                Vector3 levelDirecteion = end - new Vector3(start.X, end.Y, start.Z);
+                Vector3 right = Vector3.Cross(travelDirection, levelDirecteion);
+                Vector3 up = Vector3.Cross(right, travelDirection);
+                if (end.Y > start.Y) up = -up;
+                Vector3 computed = start + t * travelDirection;
+                up.Normalize();
+                computed += ((float)(Math.Sin((double)(t * (float)Math.PI))) * height) * up;
+                return computed;
+            }
+        }
+
+        public async Task<List<Vector3>> FindWaypoints(Vector3 target)
+        {
+            waypoints.Clear();
+            var distance = Distance2D(GameObjectManager.LocalPlayer.Location, target);
+            var desiredNumberOfPoints = Math.Max(Math.Floor(distance * Smoothing), 1);
+            var height = target.Y + NavHeight;
+            for (var i = 0.0f; i <= 1.0f; i += (1.0f / ((float)desiredNumberOfPoints)))
+            {
+                var waypoint = await SampleParabola(GameObjectManager.LocalPlayer.Location, target, (float)height, i);
+                waypoints.Add(waypoint);
+            }
+
+            return waypoints;
+        }
+
+        public async Task<bool> PathIsClear(Vector3 from, Vector3 to)
+        {
+            Vector3 hit, distances;
+            var somethingInTheWay = WorldManager.Raycast(from, to, out hit, out distances);
+
+            return !somethingInTheWay;
+        }
+
+        public async Task<bool> TakeFlight()
+        {
+            MovementManager.Jump();
+            await Coroutine.Sleep(200);
+            MovementManager.Jump();
+            await Coroutine.Sleep(200);
+
+            return true;
+        }
+
+        public async Task<bool> EnsureMounted()
+        {
+            while (!GameObjectManager.LocalPlayer.IsMounted)
+            {
+                Actionmanager.Mount(Convert.ToUInt32(MountId));
+                await Coroutine.Sleep(2000);
+            }
+            return true;
+        }
+
+        public async Task<bool> EnsureFlying()
+        {
+            await EnsureMounted();
+            if (!ff14bot.Managers.MovementManager.IsFlying)
+            {
+                await TakeFlight();
+            }
+            return true;
+        }
+
+        public async Task<bool> MoveToWithinRadius(Vector3 target, float radius)
+        {
+            while (Distance2D(GameObjectManager.LocalPlayer.Location, target) > Radius)
+            {
+                await EnsureFlying();
+                playerMover.MoveTowards(target);
+                await Coroutine.Sleep(200);
+            }
+            playerMover.MoveStop();
+            return true;
+        }
+
+        public async Task<bool> DescendAndDismount()
+        {
+            while (GameObjectManager.LocalPlayer.IsMounted)
+            {
+                Actionmanager.Dismount();
+                await Coroutine.Sleep(500);
+            }
+            return true;
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            isDone = false;
+        }
+    }
+}

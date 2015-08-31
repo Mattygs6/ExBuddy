3fc720a24d57ff3c53262bf53076181d9c552e60
Matt Gramolini <mattygs@gmail.com>
2015-08-30 20:19:22 -0400

    Map should only override when we don't have any of that map already!

--- OrderBotTags/Gather/Rotations/MapGatheringRotation.cs
+++ OrderBotTags/Gather/Rotations/MapGatheringRotation.cs
@@ -4,6 +4,7 @@ namespace ExBuddy.OrderBotTags.Gather.Rotations
     using System.Threading.Tasks;
 
     using ff14bot;
+    using ff14bot.Managers;
 
     [GatheringRotation("Map", 0, 8)]
     public class MapGatheringRotation : UnspoiledGatheringRotation
@@ -28,6 +29,12 @@ namespace ExBuddy.OrderBotTags.Gather.Rotations
         {
             // Only override if the item name ends with ' map'
             if (!tag.GatherItem.ItemData.EnglishName.EndsWith(" map", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return -1;
+            }
+
+            // Only override if we dont' have this map in our inventory
+            if (tag.GatherItem.ItemData.ItemCount() > 0)
             {
                 return -1;
             }

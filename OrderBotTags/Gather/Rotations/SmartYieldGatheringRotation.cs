d287f1f31114b924100ab91203ea657c1d7a6124
Matt Gramolini <mattygs@gmail.com>
2015-09-04 01:52:50 -0400

    Improved on logic for SmartYield

--- OrderBotTags/Gather/Rotations/SmartYieldGatheringRotation.cs
+++ OrderBotTags/Gather/Rotations/SmartYieldGatheringRotation.cs
@@ -2,30 +2,55 @@ namespace ExBuddy.OrderBotTags.Gather.Rotations
 {
     using System.Threading.Tasks;
 
+    using Buddy.Coroutines;
+
     using ff14bot;
+    using ff14bot.Managers;
 
     //Name, RequiredGp, RequiredTime
     [GatheringRotation("SmartYield", 0, 0)]
     public class SmartYieldGatheringRotation : SmartGatheringRotation, IGetOverridePriority
     {
+        private int maxGpNodeCount;
+
         public override async Task<bool> ExecuteRotation(GatherCollectableTag tag)
         {
-            if (Core.Player.CurrentGP >= 500 && Core.Player.ClassLevel >= 40)
+            var level = Core.Player.ClassLevel;
+            // Staring count at -30 since we regain that within the next node and we usually have +30 above the threshold on our gear at each point.
+            if (Core.Player.CurrentGP >= Core.Player.MaxGP - 30)
             {
-                await tag.Cast(Ability.IncreaseGatherYield2);
-                return await base.ExecuteRotation(tag);
+                maxGpNodeCount++;
             }
 
-            if (Core.Player.CurrentGP >= 400 && Core.Player.ClassLevel >= 30)
+            if (GatheringManager.SwingsRemaining > 4 ||
+                (level < 50 && maxGpNodeCount > 4) || maxGpNodeCount > 6)
             {
-                await tag.Cast(Ability.IncreaseGatherYield);
-                return await base.ExecuteRotation(tag);
-            }
+                maxGpNodeCount = 0;
 
-            if (Core.Player.CurrentGP >= 300 && Core.Player.ClassLevel >= 25)
-            {
-                await tag.Cast(Ability.AdditionalAttempt);
-                return await base.ExecuteRotation(tag);
+                if (Core.Player.CurrentGP >= 500 && level >= 40)
+                {
+                    await tag.Cast(Ability.IncreaseGatherYield2);
+                    return await base.ExecuteRotation(tag);
+                }
+
+                if (Core.Player.CurrentGP >= 400 && level >= 30 && level < 40)
+                {
+                    await tag.Cast(Ability.IncreaseGatherYield);
+                    return await base.ExecuteRotation(tag);
+                }
+
+                if (Core.Player.CurrentGP >= 300 && level >= 25 && level < 30)
+                {
+                    while (GatheringManager.ShouldPause(DataManager.SpellCache[(uint)Ability.Preparation]))
+                    {
+                        await Coroutine.Yield();
+                    }
+
+                    tag.GatherItem.GatherItem();
+
+                    await tag.Cast(Ability.AdditionalAttempt);
+                    return await base.ExecuteRotation(tag);
+                }
             }
 
             return true;

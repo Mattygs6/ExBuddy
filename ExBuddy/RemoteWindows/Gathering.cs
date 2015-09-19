6298d5cda5d38e7a2cfc18f5d7dbb4c27b2ff827
Matt Gramolini <mattygs@gmail.com>
2015-09-19 19:23:19 -0400

    Forgot some new code.

---
+++ ExBuddy/RemoteWindows/Gathering.cs
@@ -0,0 +1,10 @@
+﻿namespace ExBuddy.RemoteWindows
+{
+    public sealed class Gathering : Window<Gathering>
+    {
+        public Gathering()
+            : base("Gathering")
+        {
+        }
+    }
+}
---
+++ ExBuddy/RemoteWindows/GatheringMasterpiece.cs
@@ -0,0 +1,45 @@
+﻿namespace ExBuddy.RemoteWindows
+{
+    using ExBuddy.Enums;
+
+    using ff14bot;
+
+    public sealed class GatheringMasterpiece : Window<GatheringMasterpiece>
+    {
+        public GatheringMasterpiece()
+            : base("GatheringMasterpiece")
+        {
+        }
+
+        public static SendActionResult ClickCollect()
+        {
+            return new GatheringMasterpiece().Collect();
+        }
+
+        public SendActionResult Collect()
+        {
+            return TrySendAction(1, 1, 0);
+        }
+
+        public static int Rarity
+        {
+            get
+            {
+                return new GatheringMasterpiece().CurrentRarity;
+            }
+        }
+
+        public int CurrentRarity
+        {
+            get
+            {
+                if (IsValid)
+                {
+                    return Core.Memory.Read<int>(Control.Pointer + 0x000001C4);
+                }
+
+                return 0;
+            }
+        }
+    }
+}

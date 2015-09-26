c377dc096547b98c27d2a8357f9cb8fd058d03f2
Matt Gramolini <mattygs@gmail.com>
2015-09-26 13:11:41 -0400

    enable + disable plugins tags and re-adding tags that i left missing =( oops

---
+++ ExBuddy/OrderBotTags/Behaviors/ExDisablePluginsTag.cs
@@ -0,0 +1,63 @@
+﻿namespace ExBuddy.OrderBotTags.Behaviors
+{
+	using System;
+	using System.Linq;
+	using System.Windows.Media;
+
+	using Clio.XmlEngine;
+
+	using ExBuddy.Attributes;
+
+	using ff14bot.Managers;
+
+	[LoggerName("ExDisablePlugins")]
+	[XmlElement("ExDisablePlugins")]
+	public sealed class ExDisablePluginsTag : ExProfileBehavior
+	{
+		[XmlElement("Names")]
+		public string[] Names { get; set; }
+
+		protected override Color Info
+		{
+			get
+			{
+				return Colors.SandyBrown;
+			}
+		}
+
+		protected override void OnStart()
+		{
+			if (Names == null || Names.Length == 0)
+			{
+				isDone = true;
+				return;
+			}
+
+			StatusText = "Disabling Plugins: " + string.Join(", ", Names);
+			Logger.Info("Disabling Plugins: " + string.Join(", ", Names));
+			foreach (
+				var plugin in
+					PluginManager.Plugins.Where(p => Names.Contains(p.Plugin.Name, StringComparer.InvariantCultureIgnoreCase)))
+			{
+				try
+				{
+					if (!plugin.Enabled)
+					{
+						Logger.Info("Plugin {0} already disabled.", plugin.Plugin.Name);
+					}
+					else
+					{
+						Logger.Info("Disabling Plugin {0}", plugin.Plugin.Name);
+						plugin.Enabled = false;
+					}
+				}
+				catch (Exception ex)
+				{
+					Logger.Error(ex.Message);
+				}
+			}
+
+			isDone = true;
+		}
+	}
+}
---
+++ ExBuddy/OrderBotTags/Behaviors/ExEnablePluginsTag.cs
@@ -0,0 +1,63 @@
+﻿namespace ExBuddy.OrderBotTags.Behaviors
+{
+	using System;
+	using System.Linq;
+	using System.Windows.Media;
+
+	using Clio.XmlEngine;
+
+	using ExBuddy.Attributes;
+
+	using ff14bot.Managers;
+
+	[LoggerName("ExEnablePlugins")]
+	[XmlElement("ExEnablePlugins")]
+	public sealed class ExEnablePluginsTag : ExProfileBehavior
+	{
+		[XmlElement("Names")]
+		public string[] Names { get; set; }
+
+		protected override Color Info
+		{
+			get
+			{
+				return Colors.GreenYellow;
+			}
+		}
+
+		protected override void OnStart()
+		{
+			if (Names == null || Names.Length == 0)
+			{
+				isDone = true;
+				return;
+			}
+
+			StatusText = "Enabling Plugins: " + string.Join(", ", Names);
+			Logger.Info("Enabling Plugins: " + string.Join(", ", Names));
+			foreach (
+				var plugin in
+					PluginManager.Plugins.Where(p => Names.Contains(p.Plugin.Name, StringComparer.InvariantCultureIgnoreCase)))
+			{
+				try
+				{
+					if (plugin.Enabled)
+					{
+						Logger.Info("Plugin {0} already enabled.", plugin.Plugin.Name);
+					}
+					else
+					{
+						Logger.Info("Enabling Plugin {0}", plugin.Plugin.Name);
+						plugin.Enabled = true;
+					}
+				}
+				catch (Exception ex)
+				{
+					Logger.Error(ex.Message);
+				}
+			}
+
+			isDone = true;
+		}
+	}
+}

937b50eaf37aa3e1bfb44cbde8128172652dec79
Matt Gramolini <mattygs@gmail.com>
2015-09-28 16:49:53 -0400

    Yep..forgot the file.

---
+++ ExBuddy/OrderBotTags/Behaviors/ExLogTag.cs
@@ -0,0 +1,93 @@
+﻿namespace ExBuddy.OrderBotTags.Behaviors
+{
+	using System;
+	using System.Threading.Tasks;
+	using System.Windows.Media;
+
+	using Clio.XmlEngine;
+
+	using ExBuddy.Attributes;
+
+	[LoggerName("ExLog")]
+	[XmlElement("ExLog")]
+	public class ExLogTag : ExProfileBehavior
+	{
+		protected override Color Info
+		{
+			get
+			{
+				if (Color.HasValue)
+				{
+					return Color.Value;
+				}
+
+				return base.Info;
+			}
+		}
+
+		protected override void OnStart()
+		{
+			if (!string.IsNullOrWhiteSpace(Name))
+			{
+				Logger.Name = Name;	
+			}
+
+			Logger.IncludeVersion = false;
+		}
+
+		protected override async Task<bool> Main()
+		{
+			if (!string.IsNullOrWhiteSpace(Message))
+			{
+				Logger.Info(Message);
+			}
+			else if (!string.IsNullOrWhiteSpace(Body))
+			{
+				var lines = Body.Split(new [] { Environment.NewLine }, StringSplitOptions.None);
+
+				foreach (var line in lines)
+				{
+					Logger.Info(line);
+				}
+			}
+
+			return isDone = true;
+		}
+
+		[XmlElement("Message")]
+		public string Body { get; set; }
+
+		[XmlAttribute("Message")]
+		public string Message { get; set; }
+
+		[System.Xml.Serialization.XmlIgnore]
+		public Color? Color { get; set; }
+
+		[XmlAttribute("Color")]
+		public string ColorString
+		{
+			get
+			{
+				return Color.ToString();
+			}
+
+			set
+			{
+				try
+				{
+					Color = ColorConverter.ConvertFromString(value) as Color?;
+				}
+				catch (Exception ex)
+				{
+					Logger.Error(ex.Message);
+				}
+				
+			}
+		}
+
+		public override string ToString()
+		{
+			return "ExLogTag";
+		}
+	}
+}

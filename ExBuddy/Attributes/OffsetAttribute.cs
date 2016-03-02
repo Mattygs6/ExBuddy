de108e6a64eb03321826b66683ea7c08a4b81b0e
Tx21234 <Tx21234@gmail.com>
2016-03-01 21:15:57 -0500

    Add x64 offset type

--- ExBuddy/Attributes/OffsetAttribute.cs
+++ ExBuddy/Attributes/OffsetAttribute.cs
@@ -22,7 +22,30 @@
 		}
 	}
 
-	[AttributeUsage(AttributeTargets.All)]
+    /// <summary>
+    /// Make it a different type so its easier to grab
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class Offset64 : Attribute
+    {
+        public bool IsOffset;
+
+        public int Modifier;
+
+        public bool MultipleResults;
+
+        public string Pattern;
+
+        public Offset64(string pattern, bool isoffset = false, int modifier = 0, bool multresults = false)
+        {
+            Pattern = pattern;
+            IsOffset = isoffset;
+            Modifier = modifier;
+            MultipleResults = multresults;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
 	public class OffsetCN : Offset
 	{
 		public OffsetCN(string pattern, bool isoffset = false, int modifier = 0, bool multresults = false)

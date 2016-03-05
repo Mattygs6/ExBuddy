f8fe2574eefa4a4420f570e41fa2afeab3c75057
whowhopipi <whowhopipi@163.com>
2016-03-06 02:00:14 +0800

    制作增加中文配方支持

--- ExBuddy/OrderBotTags/Craft/ExSynthesize.cs
+++ ExBuddy/OrderBotTags/Craft/ExSynthesize.cs
@@ -1,10 +1,18 @@
 ﻿namespace ExBuddy.OrderBotTags.Craft
 {
+    using Buddy.Coroutines;
     using Clio.XmlEngine;
 
     using ExBuddy.Attributes;
+    using ff14bot;
+    using ff14bot.Behavior;
+    using ff14bot.Managers;
     using ff14bot.NeoProfiles.Tags;
     using Providers;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using TreeSharp;
+
     [LoggerName("ExSynthesize")]
     [XmlElement("ExSynthesize")]
     public sealed class ExSynthesize : Synthesize
@@ -12,18 +20,81 @@
         [XmlAttribute("Name")]
         private string Name { set; get; }
 
+        [XmlAttribute("CnName")]
+        private string CnName { set; get; }
+
         protected override void OnStart()
         {
-            if(base.RecipeId == 0 && Name != null)
+            
+            if (base.RecipeId == 0)
             {
                 Log("加载前配方ID：" + base.RecipeId);
-                uint id = RecipeProvider.Instance.GetRecipeIdByEnName(Name);
-                Log("通过名称({0})获取配方ID：{1}",Name,id);
-                if(id > 0)
+
+
+                XmlElement data = RecipeProvider.Instance.data;
+                string xpath = null;
+
+                if(Name != null)
                 {
-                    base.RecipeId = id;
+                    xpath = string.Format("/Recipes/Recipe[@Job='{0}']/recipe[@enname='{1}']/@id", Core.Me.CurrentJob, Name);
+                } else if(CnName != null)
+                {
+                    xpath = string.Format("/Recipes/Recipe[@Job='{0}']/recipe[@cnname='{1}']/@id", Core.Me.CurrentJob, CnName);
+                }
+
+                if(xpath != null)
+                {
+                    Log("xpath:{0}", xpath);
+
+                    XmlNode element = data.SelectSingleNode(xpath);
+                    if (element != null)
+                    {
+                        Log("找到一个节点：{0}", element.Value);
+
+                        base.RecipeId = uint.Parse(element.Value);
+                    }
+                    else
+                    {
+                        Log("没有找到节点");
+                    }
                 }
             }
+
+            base.OnStart();
         }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(ctx => doExStart());
+        }
+
+        private async Task<bool> doExStart()
+        {
+            bool flag = true;
+            flag = await Land() && await base.StartCrafting();
+            return flag;
+        }
+
+        private async Task<bool> Land()
+        {
+            
+            var result = true;
+            if (MovementManager.IsFlying)
+            {
+                Log("处于飞行状态，准备降落");
+                result = await CommonTasks.Land();
+                await Coroutine.Sleep(1000);
+            }
+
+            if (result && Core.Player.IsMounted)
+            {
+                Log("处于骑乘状态，准备去掉骑乘");
+                await CommonTasks.StopAndDismount();
+                await Coroutine.Sleep(1000);
+            }
+
+            return result;
+        }
+        
     }
 }
--- ExBuddy/Providers/RecipeProvider.cs
+++ ExBuddy/Providers/RecipeProvider.cs
@@ -1,118 +1,84 @@
 ﻿namespace ExBuddy.Providers
 {
+    using ff14bot.Enums;
+    using ff14bot.Managers;
+    using Logging;
     using System;
     using System.IO;
     using System.Linq;
-    using System.Xml.Linq;
-
-    using ff14bot.Managers;
-    using System.Collections.Generic;
-    using ff14bot.Enums;
-    using ff14bot;
-    using OrderBotTags.Objects;
+    using System.Xml;
     public class RecipeProvider
     {
 		public static readonly string DataFilePath;
 
 		public static readonly RecipeProvider Instance;
+        
+        public readonly XmlElement data;
 
-        // 英文配方
-        private Dictionary<ClassJobType, string> enRecipes;
-        // 中文配方
-        private Dictionary<ClassJobType, string> cnRecipes;
-
-
-		static RecipeProvider()
+        static RecipeProvider()
 		{
-            Logging.Logger.Instance.Info("初始化配方");
-			var path = Path.Combine(Environment.CurrentDirectory, "Plugins\\ExBuddy\\Data\\Recipes");
+            Logger.Instance.Info("初始化配方");
 
-			if (File.Exists(path))
-			{
-                Logging.Logger.Instance.Info("路径存在");
-                DataFilePath = path;
-			} else
+            var path = Path.Combine(Environment.CurrentDirectory, "Plugins\\ExBuddy\\Data\\recipes.xml");
+
+            if (File.Exists(path))
             {
-                Logging.Logger.Instance.Info("路径不存在");
+                DataFilePath = path;
+            }
+            else
+            {
+                DataFilePath =
+                    Directory.GetFiles(PluginManager.PluginDirectory, "*recipes.xml", SearchOption.AllDirectories).FirstOrDefault();
             }
 
 			Instance = new RecipeProvider(DataFilePath);
 		}
 
 		public RecipeProvider(string filePath)
-		{
-			if (!File.Exists(filePath))
-			{
-				return;
-			}
-
-            Dictionary<ClassJobType, string> jobs = new Dictionary<ClassJobType, string>
+        {
+            if (!File.Exists(filePath))
             {
-                {ClassJobType.Alchemist, "recipes_alchemist.txt"},
-                {ClassJobType.Armorer, "recipes_armorer.txt"},
-                {ClassJobType.Blacksmith, "recipes_blacksmith.txt"},
-                {ClassJobType.Carpenter, "recipes_carpenter.txt"},
-                {ClassJobType.Culinarian, "recipes_culinarian.txt"},
-                {ClassJobType.Goldsmith, "recipes_goldsmith.txt"},
-                {ClassJobType.Leatherworker, "recipes_leatherworker.txt"},
-                {ClassJobType.Weaver, "recipes_weaver.txt"}
-            };
+                return;
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
 
-            //Dictionary<ClassJobType, Directory> recipe;
-            
+            data = doc.DocumentElement;
+
         }
 
 		public bool IsValid
 		{
 			get
-			{
-				return enRecipes != null || cnRecipes != null;
-			}
+            {
+                return data != null;
+            }
 		}
 
-        public uint GetRecipeIdByEnName(string recipeName)
+        public uint GetRecipeIdByEnName(string recipeName,ClassJobType job)
         {
-            Logging.Logger.Instance.Info("获取配方{0}",recipeName);
+            Logger.Instance.Info("获取配方{0}-{1}",job,recipeName);
             if (IsValid)
             {
                 return 0;
             }
 
-            string recipeList = enRecipes[Core.Me.CurrentJob];
+            string xpath = string.Format("/Recipes/Recipe[@Job='{0}']/recipe[@enname='{1}']/@id", job, recipeName);
+            Logger.Instance.Info("xpath:{0}", xpath);
 
-            string jobPath = string.Format("{0}\\{1}", DataFilePath, recipeList);
-            Logging.Logger.Instance.Info("当前配方地址{0}", jobPath);
+            XmlNode element = data.SelectSingleNode(xpath);
 
-            // Add the lines of the read file to the list variable.
-            List<string> rawRecipeList = File.ReadLines(jobPath).Where(line => line != null).ToList();
-
-            // Create a Recipe object for each item inside the rawRecipeList and store them in the recipes list.
-            foreach (string recipe in rawRecipeList)
+            if(element != null)
             {
-                Recipe recipeObj = ParseRecipe(recipe);
-                if (recipeObj != null)
-                {
-                    if (recipeObj.Name == recipeName)
-                        return recipeObj.Id;
-                }
+                Logger.Instance.Info("找到节点：{0}", element.Value);
+                return uint.Parse(element.Value);
+            } else
+            {
+                Logger.Instance.Error("没有找到节点{0}-{1}", job, recipeName);
             }
 
             return 0;
         }
-
-        private Recipe ParseRecipe(string recipeRaw)
-        {
-            if (recipeRaw.IndexOf(':') == -1)
-                return null;
-            string[] recipeSplit = recipeRaw.Split(':');
-            if (recipeSplit.Length != 2) return null;
-
-            uint recipeId;
-            string recipeName = recipeSplit[1].Substring(1);
-            if (uint.TryParse(recipeSplit[0], out recipeId))
-                return new Recipe(recipeId, recipeName.Replace("-", " "));
-
-            return null;
-        }
+        
 	}
 }

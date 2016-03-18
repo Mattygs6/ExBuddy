2c48b9dcd7a632f7e1302b9c0ed5b76bbbc29b3e
7h3Cub3 <7h3Cub3@gmail.com>
2016-03-18 09:58:22 +0100

    Data separation for RB_CN client
    - Added separation between chinese client and the rest for all datas

--- ExBuddy/OrderBotTags/Behaviors/Objects/Data.cs
+++ ExBuddy/OrderBotTags/Behaviors/Objects/Data.cs
@@ -92,6 +92,208 @@ namespace ExBuddy.OrderBotTags.Behaviors.Objects
 
 		public static readonly Dictionary<ShopItem, ShopItemInfo> ShopItemMap = new Dictionary<ShopItem, ShopItemInfo>
 																					{
+																						
+																						#if RB_CN
+																						{
+																							ShopItem.CrpDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.CrpDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.CrpDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.BsmDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.BsmDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.BsmDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.ArmDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.ArmDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.ArmDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.GsmDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.GsmDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.GsmDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.LtwDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.LtwDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.LtwDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.WvrDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.WvrDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.WvrDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.AlcDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.AlcDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.AlcDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.CulDelineation,
+																							new ShopItemInfo
+																								{
+																									Index = 8 + (int)ShopItem.CulDelineation,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12667 + (int)ShopItem.CulDelineation,
+																									Cost = 250,
+																									Yield = 10
+																								}
+																						},
+																						{
+																							ShopItem.RedCrafterToken,
+																							new ShopItemInfo
+																								{
+																									Index = 0,
+																									ShopType = ShopType.RedCrafter,
+																									ItemId = 12838,
+																									Cost = 50,
+																									Yield = 1
+																								}
+																						},
+																						{
+																							ShopItem.RedGatherToken,
+																							new ShopItemInfo
+																								{
+																									Index = 0,
+																									ShopType = ShopType.RedGatherer,
+																									ItemId = 12840,
+																									Cost = 50,
+																									Yield = 1
+																								}
+																						},
+																						{
+																							ShopItem.HiCordial,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.HiCordial,
+																									ShopType = ShopType.BlueGatherer,
+																									ItemId = 12669,
+																									Cost = 100,
+																									Yield = 1
+																								}
+																						},
+																						{
+																							ShopItem.BlueToken,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.BlueToken,
+																									ShopType = ShopType.BlueGatherer,
+																									ItemId = 12841,
+																									Cost = 250,
+																									Yield = 5
+																								}
+																						},
+																						{
+																							ShopItem.BruteLeech,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.BruteLeech,
+																									ShopType = ShopType.BlueGatherer,
+																									ItemId = 12711,
+																									Cost = 60,
+																									Yield = 50
+																								}
+																						},
+																						{
+																							ShopItem.CraneFly,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.CraneFly,
+																									ShopType = ShopType.BlueGatherer,
+																									ItemId = 12712,
+																									Cost = 60,
+																									Yield = 50
+																								}
+																						},
+																						{
+																							ShopItem.KukuruPowder,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.KukuruPowder,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12886,
+																									Cost = 50,
+																									Yield = 1
+																								}
+																						},
+																						{
+																							ShopItem.BouillonCube,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.BouillonCube,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12905,
+																									Cost = 40,
+																									Yield = 5
+																								}
+																						},
+																						{
+																							ShopItem.BeanSauce,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.BeanSauce,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12906,
+																									Cost = 30,
+																									Yield = 1
+																								}
+																						},
+																						{
+																							ShopItem.BeanPaste,
+																							new ShopItemInfo
+																								{
+																									Index = (int)ShopItem.BeanPaste,
+																									ShopType = ShopType.BlueCrafter,
+																									ItemId = 12907,
+																									Cost = 30,
+																									Yield = 1
+																								}
+																						}
+																										
+																						#else
 																						{
 																							ShopItem.CrpDelineation,
 																							new ShopItemInfo
@@ -400,6 +602,7 @@ namespace ExBuddy.OrderBotTags.Behaviors.Objects
 																									Yield = 5
 																								}
 																						}
+																						#endif
 																					};
 
 		public static IEnumerable<INpc> GetNpcsByLocation(Locations location)

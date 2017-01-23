bd3f95e7d72679e1ff7baaa20b1ec2110e3769bd
Entrax <entrax.643@gmail.com>
2017-01-23 23:46:15 +0100

    New Rotations
    - All rotation used for Ephemeral and Collectable items that are always
    available (Tektite, Cloud Mushroom..)
    
    - Specific element rotation for Revenant's toll Unspoiled
    
    - Yield and Quality rotation updated to be used with 600, 650 or 700 GP.

---
+++ ExBuddy/OrderBotTags/Gather/Rotations/EarthElementGatheringRotation.cs
@@ -0,0 +1,22 @@
+﻿namespace ExBuddy.OrderBotTags.Gather.Rotations
+{
+    using System.Threading.Tasks;
+    using Attributes;
+    using ff14bot;
+    using ff14bot.Managers;
+
+    [GatheringRotation("EarthElement", 30, 400)]
+    public sealed class EarthElementGatheringRotation : SmartGatheringRotation
+    {
+        public override async Task<bool> ExecuteRotation(ExGatherTag tag)
+        {
+            if (Core.Player.CurrentGP > 399)
+            {
+                await Wait();
+                Actionmanager.DoAction(217U, Core.Player);
+            }
+
+            return await base.ExecuteRotation(tag);
+        }
+    }
+}
---
+++ ExBuddy/OrderBotTags/Gather/Rotations/WaterElementGatheringRotation.cs
@@ -0,0 +1,22 @@
+﻿namespace ExBuddy.OrderBotTags.Gather.Rotations
+{
+    using System.Threading.Tasks;
+    using Attributes;
+    using ff14bot;
+    using ff14bot.Managers;
+
+    [GatheringRotation("WaterElement", 30, 400)]
+    public sealed class WaterElementGatheringRotation : SmartGatheringRotation
+    {
+        public override async Task<bool> ExecuteRotation(ExGatherTag tag)
+        {
+            if (Core.Player.CurrentGP > 399)
+            {
+                await Wait();
+                Actionmanager.DoAction(293U, Core.Player);
+            }
+            
+            return await base.ExecuteRotation(tag);
+        }
+    }
+}
